fdfc22c8ccb1e9eeefdc1801d8e828222533dca6
christinezosche <christine.zosche@gmail.com>
2020-09-02 17:13:05 -0700

    WIP: example of a bad commit message

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,4 +1,8 @@
 class Property < ApplicationRecord
     has_many :bookings
     has_many :users, through: :bookings
+    validates :address, presence: true
+    def full_address
+        "#{self.address}, #{self.city}, #{self.state}" 
+    end
 end
